1b5ebd786029724992dfe26b2e4af612f75b25e3
Jed Palero <raven@developer.dev>
2023-11-20 16:00:04 +0800

    Setting up the HTML document

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Footer from "./Component/Footer";
+import Header from "./Component/Header";
+import Main from "./Component/Main";
+import Nav from "./Component/Nav";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Component/Header.js
@@ -0,0 +1,10 @@
+import logo from "../icons_assets/Logo.svg";
+
+const Header = () => {
+  return (
+    <>
+      <img src={logo} alt="little lemon logo" />
+    </>
+  );
+};
+export default Header;
