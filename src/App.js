48665765776c48407cf24cf6c76f6e659bb52acf
Jed Palero <raven@developer.dev>
2023-11-21 16:27:39 +0800

    Final edit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import Footer from "./Component/Footer";
 import Main from "./Component/Main";
+import Menu from "./Component/Menu";
 import Nav from "./Component/Nav";
 
 function App() {
   return (
     <>
       <Nav />
-
       <Main />
+      <Menu />
       <Footer />
     </>
   );
